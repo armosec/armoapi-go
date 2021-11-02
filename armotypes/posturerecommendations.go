cfb0fd4e94ffc934e951aa3b12141ecb76fe59c7
Bezalel Brandwine <bbrandwine@armosec.io>
2021-11-02 21:46:03 +0200

    add some armo context methods + unitests

--- armotypes/posturerecommendations.go
+++ armotypes/posturerecommendations.go
@@ -61,31 +61,3 @@ type RecommendationAssociation struct {
 	// current status of this recommendation for the given resource
 	Status AssociationStatus `json:"status"`
 }
-
-// {
-//     "customerGUID": "",
-//     "designators": { // designator object as we have in current resources represntaion.
-//         // limitation of this structure is that we need separate "sub-index" for each attribute and we
-//         // don't have any extra information regarding this attributes (like source, equlity and so on)
-//         "designatorsType": "attributes",
-//         "attributes": {
-//             "lbl1": "val11"
-//         }
-//     },
-//     "context": [ // more flexible structure for attributes
-//         {
-//             "attributeName": "",
-//             "attributeValue": "",
-//             "attributesource": ""
-//         }
-//     ],
-//     "actionPrototypeID": "<guid>", //guid of the recommendation in recommendation DB
-//     "status": 2, // current status of this recommendation for the given resource. (e.g. 1- attached ,2- ignored, 3- applied, 4- another remediation, ...)
-//     "updatesAudit": [
-//         {
-//             "time": "",
-//             "userName": "",
-//             "status": 3
-//         }
-//     ]
-// }
