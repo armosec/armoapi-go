80e735db23e4e9f410e7afa03f478174744c9f93
lalafi@cyberarmor.io <lalafi@cyberarmor.io>
2021-11-03 15:26:05 +0200

    kpi posture scan datastructure

--- armotypes/kpitypes.go
+++ armotypes/kpitypes.go
@@ -2,26 +2,14 @@ package armotypes
 
 import "time"
 
-type KPIPostureScanWrapper struct {
-	ReportType    string         `json:"reportType"`
-	ReportPayload KPIPostureScan `json:"reportPayload"`
-}
-
 type KPIPostureScan struct {
-	CustomerGUID string    `json:"customerGUID"`
-	ReportGUID   string    `json:"reportGUID"`
-	Frameworks   []string  `json:"frameworks"`
-	Timestamp    time.Time `json:"timestamp"`
-	ClusterName  string    `json:"clusterName"`
-	SourceType   string    `json:"sourceType"`   //yaml,helm,running - what we actually scanned
-	OutputFormat string    `json:"outputFormat"` // json,junit,prettyprint - output prefered by customers
-	CICD         int       `json:"CICD"`         // 0 -false , 1- true , 2- unknown
-	IP           string    `json:"IP,omitempty"`
-}
-
-type KPIPLoginWrapper struct {
-	ReportType    string         `json:"reportType"`
-	ReportPayload KPIPostureScan `json:"reportPayload"`
+	Client           string    `json:"client"`
+	ClientVersion    string    `json:"clientVersion"`
+	Framework        string    `json:"framework"`
+	FrameworkVersion string    `json:"frameworkVersion"`
+	Timestamp        time.Time `json:"timestamp"`
+	Target           string    `json:"target"` //yaml,helm,running - what we actually scanned
+	ClientIP         string    `json:"clientIP"`
 }
 
 type KPILogin struct {
