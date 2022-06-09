02e08ffba52f971ab5645bfb670e128042eee653
cigra <marinas@armosec.io>
2022-06-09 15:09:02 +0300

    registry types

---
+++ armotypes/registrytypes.go
@@ -0,0 +1,11 @@
+package armotypes
+
+
+type RegistryJobParams struct {
+	Name            string `json:"name,omitempty"`
+	ID              string `json:"id,omitempty"`
+	ClusterName     string `json:"clusterName"`
+	RegistryName    string `json:"registryName"`
+	CronTabSchedule string `json:"cronTabSchedule,omitempty"`
+	JobID           string `json:"jobID,omitempty"`
+}
