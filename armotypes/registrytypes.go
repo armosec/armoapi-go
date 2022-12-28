cb57a95e836ce0f372a681ba4a2fac14165d04b8
cigra <marinas@armosec.io>
2022-12-28 22:38:26 +0200

    fix bool omitempty

--- armotypes/registrytypes.go
+++ armotypes/registrytypes.go
@@ -13,12 +13,12 @@ type RegistryInfo struct {
 	RegistryName     string     `json:"registryName,omitempty" bson:"registryName"`
 	RegistryProvider string     `json:"registryProvider,omitempty" bson:"registryProvider"`
 	RegistryToken    string     `json:"registryToken,omitempty" bson:"registryToken"`
-	Depth            int        `json:"depth,omitempty" bson:"depth"`
+	Depth            *int       `json:"depth,omitempty" bson:"depth"`
 	Include          []string   `json:"include,omitempty" bson:"include"`
 	Exclude          []string   `json:"exclude,omitempty" bson:"exclude"`
 	Kind             string     `json:"kind,omitempty" bson:"kind"`
-	IsHTTPs          bool       `json:"isHTTPs" bson:"isHTTPs"`
-	SkipTLSVerify    bool       `json:"skipTLSVerify" bson:"skipTLSVerify"`
+	IsHTTPs          *bool      `json:"isHTTPs,omitempty" bson:"isHTTPs"`
+	SkipTLSVerify    *bool      `json:"skipTLSVerify,omitempty" bson:"skipTLSVerify"`
 	AuthMethod       AuthMethod `json:"authMethod,omitempty" bson:"authMethod"`
 }
 
