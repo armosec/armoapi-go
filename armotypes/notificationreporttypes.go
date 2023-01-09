2a95a12410e278320d6ffbf2715135ca6e63ace0
Daniel-GrunbergerCA <danielg@armosec.io>
2023-01-09 13:53:42 +0200

    fixes

--- armotypes/notificationreporttypes.go
+++ armotypes/notificationreporttypes.go
@@ -1,25 +1,39 @@
 package armotypes
 
 type WeeklyReport struct {
-	ClustersScannedThisWeek             int                 `json:"clustersScannedThisWeek" bson:"clustersScannedThisWeek"`
-	ClustersScannedPrevWeek             int                 `json:"clustersScannedPrevWeek" bson:"clustersScannedPrevWeek"`
-	LinkToConfigurationScanningFiltered string              `json:"linkToConfigurationScanningFiltered" bson:"linkToConfigurationScanningFiltered"`
-	RepositoriesScannedThisWeek         int                 `json:"repositoriesScannedThisWeek" bson:"repositoriesScannedThisWeek"`
-	RepositoriesScannedPrevWeek         int                 `json:"repositoriesScannedPrevWeek" bson:"repositoriesScannedPrevWeek"`
-	LinkToRepositoriesScanningFiltered  string              `json:"linkToRepositoriesScanningFiltered" bson:"linkToRepositoriesScanningFiltered"`
-	RegistriesScannedThisWeek           int                 `json:"registriesScannedThisWeek" bson:"registriesScannedThisWeek"`
-	RegistriesScannedPrevWeek           int                 `json:"registriesScannedPrevWeek" bson:"registriesScannedPrevWeek"`
-	LinkToRegistriesScanningFiltered    string              `json:"linkToRegistriesScanningFiltered" bson:"linkToRegistriesScanningFiltered"`
-	Top5FailedControls                  []TopCtrlItem       `json:"top5FailedControls" bson:"top5FailedControls"`
-	Top5FailedCVEs                      []TopVulItem        `json:"top5FailedCVEs" bson:"top5FailedCVEs"`
-	ClustersScanned                     []ClusterScanned    `json:"clustersScanned" bson:"clustersScanned"`
-	RepositoriesScanned                 []RepositoryScanned `json:"repositoriesScanned" bson:"repositoriesScanned"`
-	RegistriesScanned                   []RegistryScanned   `json:"registriesScanned" bson:"registriesScanned"`
+	ClustersScannedThisWeek             int                      `json:"clustersScannedThisWeek" bson:"clustersScannedThisWeek"`
+	ClustersScannedPrevWeek             int                      `json:"clustersScannedPrevWeek" bson:"clustersScannedPrevWeek"`
+	LinkToConfigurationScanningFiltered string                   `json:"linkToConfigurationScanningFiltered" bson:"linkToConfigurationScanningFiltered"`
+	RepositoriesScannedThisWeek         int                      `json:"repositoriesScannedThisWeek" bson:"repositoriesScannedThisWeek"`
+	RepositoriesScannedPrevWeek         int                      `json:"repositoriesScannedPrevWeek" bson:"repositoriesScannedPrevWeek"`
+	LinkToRepositoriesScanningFiltered  string                   `json:"linkToRepositoriesScanningFiltered" bson:"linkToRepositoriesScanningFiltered"`
+	RegistriesScannedThisWeek           int                      `json:"registriesScannedThisWeek" bson:"registriesScannedThisWeek"`
+	RegistriesScannedPrevWeek           int                      `json:"registriesScannedPrevWeek" bson:"registriesScannedPrevWeek"`
+	LinkToRegistriesScanningFiltered    string                   `json:"linkToRegistriesScanningFiltered" bson:"linkToRegistriesScanningFiltered"`
+	Top5FailedControls                  []TopCtrlItem            `json:"top5FailedControls" bson:"top5FailedControls"`
+	Top5FailedCVEs                      []TopVulItem             `json:"top5FailedCVEs" bson:"top5FailedCVEs"`
+	ClustersScanned                     []ClusterResourceScanned `json:"clustersScanned" bson:"clustersScanned"`
+	RepositoriesScanned                 []RepositoryScanned      `json:"repositoriesScanned" bson:"repositoriesScanned"`
+	RegistriesScanned                   []RegistryScanned        `json:"registriesScanned" bson:"registriesScanned"`
 }
 
-type ClusterScanned struct {
-	Cluster ClusterResourceScanned `json:"cluster" bson:"cluster"`
+type NotificationsConfig struct {
+	//Map of unsubscribed user id to notification config identifier
+	UnsubscribedUsers  map[string]NotificationConfigIdentifier `json:"unsubscribedUsers,omitempty" bson:"unsubscribedUsers,omitempty"`
+	LatestWeeklyReport *WeeklyReport                           `json:"latestWeeklyReport,omitempty" bson:"latestWeeklyReport,omitempty"`
 }
+
+type NotificationConfigIdentifier struct {
+	NotificationType NotificationType `json:"notificationType,omitempty" bson:"notificationType,omitempty"`
+}
+
+type NotificationType string
+
+const (
+	NotificationTypeAll    NotificationType = "all"
+	NotificationTypePush   NotificationType = "push"
+	NotificationTypeWeekly NotificationType = "weekly"
+)
 
 type RegistryScanned struct {
 	Registry ResourceScanned `json:"registry" bson:"registry"`
@@ -30,14 +44,13 @@ type RepositoryScanned struct {
 }
 
 type ClusterResourceScanned struct {
-	FullName  string                     `json:"fullName" bson:"fullName"`
-	ShortName string                     `json:"shortName" bson:"shortName"`
-	Resource  map[string]ResourceDetails `json:"resourcesToDetails" bson:"resourcesToDetails"`
+	ShortName string          `json:"shortName" bson:"shortName"`
+	Cluster   ResourceScanned `json:"cluster" bson:"cluster"`
 }
 
 type ResourceScanned struct {
-	Name     string                     `json:"name" bson:"name"`
-	Resource map[string]ResourceDetails `json:"resourcesToDetails" bson:"resourcesToDetails"`
+	Name                         string                     `json:"name" bson:"name"`
+	MapSeverityToResourceDetails map[string]ResourceDetails `json:"resourcesToDetails" bson:"resourcesToDetails"`
 }
 
 type ResourceDetails struct {
