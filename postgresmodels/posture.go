35e8d04d4145c0b3052fec75c78604ad2fc1ff3b
Avraham Shalev <8184528+avrahams@users.noreply.github.com>
2023-06-11 09:40:25 +0300

    changep1
    Signed-off-by: Avraham Shalev <8184528+avrahams@users.noreply.github.com>

--- postgresmodels/posture.go
+++ postgresmodels/posture.go
@@ -1,6 +1,9 @@
 package postgresmodels
 
 import (
+	"database/sql/driver"
+	"time"
+
 	"github.com/lib/pq"
 	"gorm.io/datatypes"
 )
@@ -9,9 +12,9 @@ import (
 
 type Cluster struct {
 	BaseModel
-	GUID          string `gorm:"primaryKey"`
-	CustomerGuid  string
-	Location      string
+	GUID         string `gorm:"primaryKey"`
+	CustomerGuid string
+	//Location      string
 	CloudProvider string
 	FullName      string
 	ShortName     string
@@ -19,21 +22,23 @@ type Cluster struct {
 }
 
 type ClusterPostureReport struct {
-	ReportGUID          string `gorm:"primaryKey"`
-	ClusterGUID         string
-	Cluster             Cluster `gorm:"foreignKey:ClusterGUID"`
-	Score               float32
-	WorkerNodeCount     int
-	KubescapeVersion    string
-	KubernetesVersion   string
-	HelmVersion         string
-	RegoLibraryVersion  string
+	ReportGUID        string `gorm:"primaryKey"`
+	ClusterGUID       string
+	Cluster           Cluster `gorm:"foreignKey:ClusterGUID"`
+	Score             float32
+	Timestamp         time.Time
+	WorkerNodeCount   int
+	KubescapeVersion  string
+	KubernetesVersion string
+	HelmChartVersion  string
+	//RegoLibraryVersion  string
 	ControlsSummary     ControlsSummary      `gorm:"foreignKey:ReportGUID"`
 	ControlsScanResults []ControlScanResult  `gorm:"foreignKey:ReportGUID"`
 	FrameworksSummary   []FrameworkSummary   `gorm:"foreignKey:ReportGUID"`
 	ResourceScanResults []ResourceScanResult `gorm:"foreignKey:ReportGUID"`
 }
 
+/*
 type Control struct {
 	BaseModel
 	ControlID          string `gorm:"primaryKey"`
@@ -42,25 +47,29 @@ type Control struct {
 	Description        string
 	Remediation        string
 	BaseScore          float32
-}
+}*/
 
 type ControlScanResult struct {
 	BaseModel
-	ControlID          string    `gorm:"primaryKey"`
-	ReportGUID         string    `gorm:"primaryKey"`
-	FrameworkGUID      string    `gorm:"primaryKey"`
-	Framework          Framework `gorm:"foreignKey:FrameworkGUID"`
-	Control            Control   `gorm:"foreignKey:ControlID,RegoLibraryVersion"`
-	RegoLibraryVersion string
-	Status             string
-	SubStatus          string
+	ControlID                  string `gorm:"primaryKey"`
+	ReportGUID                 string `gorm:"primaryKey"`
+	FrameworkName              string `gorm:"primaryKey"`
+	ControlGUID                string
+	Name                       string
+	Status                     string
+	SubStatus                  string
+	ComplianceScore            float32
+	AffectedResourcesCount     int
+	FailedResourcesCount       int
+	SkippedResourcesCount      int
+	TotalScannedResourcesCount int
 }
 
 type ControlsSummary struct {
 	ReportGUID               string `gorm:"primaryKey"`
 	TotalControls            int
 	FailedControls           int
-	WarningControls          int
+	SkippedControls          int
 	CriticalSeverityControls int
 	HighSeverityControls     int
 	MediumSeverityControls   int
@@ -80,50 +89,46 @@ type Resource struct {
 	RelatedNamespaces pq.StringArray `gorm:"type:text[]"`
 }
 
+/*
 type Framework struct {
 	BaseModel
 	GUID         string `gorm:"primaryKey"`
 	Name         string
 	CustomerGuid string
-}
-
-type FrameworkControl struct {
-	BaseModel
-	ControlID          string    `gorm:"primaryKey"`
-	FrameworkGUID      string    `gorm:"primaryKey"`
-	Framework          Framework `gorm:"foreignKey:FrameworkGUID"`
-	RegoLibraryVersion string
-	Control            Control `gorm:"foreignKey:ControlID,RegoLibraryVersion"`
-}
+}*/
 
 type FrameworkSummary struct {
 	BaseModel
-	ReportGUID       string    `gorm:"primaryKey"`
-	FrameworkGUID    string    `gorm:"primaryKey"`
-	Framework        Framework `gorm:"foreignKey:FrameworkGUID"`
-	RiskScore        float32
-	ImprovementScore float32
+	ReportGUID    string `gorm:"primaryKey"`
+	FrameworkName string `gorm:"primaryKey"`
+	//Framework       Framework `gorm:"foreignKey:FrameworkGUID"`
+	ComplianceScore float32
+	TotalControls   int `json:"totalControls"`
+	FailedControls  int `json:"failedControls"`
+	SkippedControls int `json:"skippedControls,omitempty"`
+
+	//Designators      PortalDesignator `json:"designators"`
 }
 
 type ResourceScanResult struct {
 	BaseModel
-	ResourceID         string `gorm:"primaryKey"`
-	ReportGUID         string `gorm:"primaryKey"`
-	FrameworkGUID      string `gorm:"primaryKey"`
-	ControlID          string `gorm:"primaryKey"`
-	RegoLibraryVersion string
-	Control            Control   `gorm:"foreignKey:ControlID,RegoLibraryVersion"`
-	Resource           Resource  `gorm:"foreignKey:ResourceID,ReportGUID"`
-	Framework          Framework `gorm:"foreignKey:FrameworkGUID"`
-	Status             string
-	ExceptionApplied   datatypes.JSON
-	FixPaths           datatypes.JSON
+	ResourceID    string `gorm:"primaryKey"`
+	ReportGUID    string `gorm:"primaryKey"`
+	FrameworkName string `gorm:"primaryKey"`
+	ControlID     string `gorm:"primaryKey"`
+	ControlName   string
+	//RegoLibraryVersion string
+	//Control            Control   `gorm:"foreignKey:ControlID,RegoLibraryVersion"`
+	Resource Resource `gorm:"foreignKey:ResourceID,ReportGUID"`
+	//Framework        Framework `gorm:"foreignKey:FrameworkGUID"`
+	Status           string
+	ExceptionApplied datatypes.JSON
+	FixPaths         datatypes.JSON
 }
 
 type Designators struct {
 }
 
-/*
 type Ecosystem string
 
 const (
@@ -141,4 +146,4 @@ func (e Ecosystem) Value() (driver.Value, error) {
 }
 
 // model define
-Ecosystem        Ecosystem `json:"ecosystem" sql:"type:ENUM('production', 'testsystem')"`*/
+//Ecosystem        Ecosystem `json:"ecosystem" sql:"type:ENUM('production', 'testsystem')"`*/
