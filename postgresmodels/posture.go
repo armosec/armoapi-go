0ae00758a190f50c71709a62f189985c45da68c5
Avraham Shalev <8184528+avrahams@users.noreply.github.com>
2023-08-08 08:28:40 +0300

    pr update
    Signed-off-by: Avraham Shalev <8184528+avrahams@users.noreply.github.com>

--- postgresmodels/posture.go
+++ postgresmodels/posture.go
@@ -7,46 +7,32 @@ import (
 	"gorm.io/datatypes"
 )
 
-// TODO: add explicit column names, add validation
-
-type Cluster struct {
+type PostureReportStatus struct {
 	BaseModel
-	GUID         string `gorm:"primaryKey"`
-	CustomerGuid string
-	//Location      string
-	CloudProvider string
-	FullName      string
-	ShortName     string
-	PrefixName    string
+	ReportGUID string `gorm:"primaryKey"`
+	BaseReport
 }
-
 type ClusterPostureReport struct {
-	ReportGUID        string `gorm:"primaryKey"`
-	ClusterGUID       string
-	Cluster           Cluster `gorm:"foreignKey:ClusterGUID"`
-	Score             float32
-	Timestamp         time.Time
-	WorkerNodeCount   int
-	KubescapeVersion  string
-	KubernetesVersion string
-	HelmChartVersion  string
-	//RegoLibraryVersion  string
-	ControlsSummary     ControlsSummary      `gorm:"foreignKey:ReportGUID"`
-	ControlsScanResults []ControlScanResult  `gorm:"foreignKey:ReportGUID"`
-	FrameworksSummary   []FrameworkSummary   `gorm:"foreignKey:ReportGUID"`
-	ResourceScanResults []ResourceScanResult `gorm:"foreignKey:ReportGUID"`
-}
-
-/*
-type Control struct {
 	BaseModel
-	ControlID          string `gorm:"primaryKey"`
-	RegoLibraryVersion string `gorm:"primaryKey"`
-	Name               string //TODO: primary key?
-	Description        string
-	Remediation        string
-	BaseScore          float32
-}*/
+	ReportGUID               string `gorm:"primaryKey"`
+	ClusterGUID              string
+	ClusterName              string
+	CustomerGUID             string
+	Score                    float32
+	Timestamp                time.Time
+	WorkerNodeCount          int
+	KubescapeVersion         string
+	KubernetesVersion        string
+	HelmChartVersion         string
+	RegoLibraryVersion       string
+	TotalControls            int
+	FailedControls           int
+	SkippedControls          int
+	CriticalSeverityControls int
+	HighSeverityControls     int
+	MediumSeverityControls   int
+	LowSeverityControls      int
+}
 
 type ControlScanResult struct {
 	BaseModel
@@ -63,23 +49,6 @@ type ControlScanResult struct {
 	SkippedResourcesCount      int
 	WarningResourcesCount      int
 	TotalScannedResourcesCount int
-
-	//HighlightPathsCount        int64
-	//ControlInputs                  []ControlInputs
-	//FrameworkSubSectionID          []string
-}
-
-type ControlsSummary struct {
-	BaseModel
-	BaseReport                      //we keep it here and not on the cluster summary for security framework scans
-	ReportGUID               string `gorm:"primaryKey"`
-	TotalControls            int
-	FailedControls           int
-	SkippedControls          int
-	CriticalSeverityControls int
-	HighSeverityControls     int
-	MediumSeverityControls   int
-	LowSeverityControls      int
 }
 
 type FrameworkSummary struct {
@@ -90,47 +59,37 @@ type FrameworkSummary struct {
 	TotalControls   int
 	FailedControls  int
 	SkippedControls int
-	TypeTags        []string `gorm:"type:text[]"`
-	//Designators      PortalDesignator `json:"designators"`
+	TypeTags        pq.StringArray `gorm:"type:text[]"`
 }
 
 type Resource struct {
 	BaseModel
-	ResourceID string `gorm:"primaryKey"`
-	ReportGUID string `gorm:"primaryKey"`
-	//Designators                datatypes.JSON
-	EventId           []byte
-	Kind              string
-	Name              string
-	Namespace         string
-	RelatedNames      pq.StringArray `gorm:"type:text[]"`
-	RelatedKinds      pq.StringArray `gorm:"type:text[]"`
-	RelatedNamespaces pq.StringArray `gorm:"type:text[]"`
+	ResourceID        string         `gorm:"primaryKey"`
+	ReportGUID        string         `gorm:"primaryKey"`
+	Designators       datatypes.JSON `gorm:"type:json"` //Portal designators
+	ResourceObjectRef string         //external storage ref(e.g. S3 bucket:key) to the resource file
+	HighlightsPaths   datatypes.JSON `gorm:"type:json"` //map controlID to []PosturePaths
 }
 
-type ResourceScanResult struct {
+type ResourceControlResult struct {
 	BaseModel
-	ResourceID      string         `gorm:"primaryKey"`
-	ReportGUID      string         `gorm:"primaryKey"`
-	FrameworkName   string         `gorm:"primaryKey"`
-	Resource        Resource       `gorm:"foreignKey:ResourceID,ReportGUID"`
-	FailedControl   pq.StringArray `gorm:"type:text[]"`
-	WarningControls pq.StringArray `gorm:"type:text[]"`
-	SkippedControls pq.StringArray `gorm:"type:text[]"`
-	//maps statusText 2 list of controlIDs
-	StatusToControls         datatypes.JSON `gorm:"type:json"`
-	HighlightsPerCtrl        datatypes.JSON `gorm:"type:json"`
-	FailedControlCount       int
-	SkippedControlCount      int
-	WarningControlCount      int
-	Status                   int
-	StatusText               string
-	SubStatusText            string
-	ExceptionApplied         datatypes.JSON `gorm:"type:json"`
-	Images                   datatypes.JSON `gorm:"type:json"`
-	ControlsInfo             datatypes.JSON `json:"controlsInfo"`
-	CriticalSeverityControls int
-	HighSeverityControls     int
-	MediumSeverityControls   int
-	LowSeverityControls      int
+	ResourceID          string   `gorm:"primaryKey"`
+	ReportGUID          string   `gorm:"primaryKey"`
+	FrameworkName       string   `gorm:"primaryKey"`
+	ControlID           string   `gorm:"primaryKey"`
+	Resource            Resource `gorm:"foreignKey:ResourceID,ReportGUID"`
+	StatusCode          int
+	StatusText          string
+	SubStatusText       string
+	IgnoreRulesIDs      pq.StringArray `gorm:"type:text[]"`
+	SystemRulesNames    pq.StringArray `gorm:"type:text[]"`
+	RelatedResourcesIDs pq.StringArray `gorm:"type:text[]"`
+}
+
+type ResourceContainer struct {
+	BaseModel
+	ResourceID    string `gorm:"primaryKey"`
+	ReportGUID    string `gorm:"primaryKey"`
+	ContainerName string `gorm:"primaryKey"`
+	ImageTag      string
 }
