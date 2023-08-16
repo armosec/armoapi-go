825a7ff14765e68a24e49e05fa5b25e5f54f1162
kooomix <eranm@armosec.io>
2023-08-16 10:09:42 +0300

    support in attach chain states payloads

--- armotypes/attackchainstypes.go
+++ armotypes/attackchainstypes.go
@@ -55,3 +55,24 @@ type AttackChainUIStatus struct {
 	ViewedMainScreen string `json:"wasViewedMainScreen,omitempty" bson:"wasViewedMainScreen,omitempty"` // if the attack chain was viewed by the user// New badge
 	ProcessingStatus string `json:"processingStatus,omitempty" bson:"processingStatus,omitempty"`       // "processing"/ "done"
 }
+
+// struct for ConsumerAttackChainsStatesUpdate ingester as payloads
+type AttackChainFirstSeen struct {
+	AttackChainID    string `json:"attackChainID,omitempty" bson:"attackChainID,omitempty"` // name/cluster/resourceID
+	CustomerGUID     string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
+	ViewedMainScreen string `json:"viewedMainScreen,omitempty" bson:"viewedMainScreen,omitempty"`
+}
+
+type AttackChainScanStatus struct {
+	ClusterName      string `json:"clusterName,omitempty" bson:"clusterName,omitempty"`
+	CustomerGUID     string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
+	ProcessingStatus string `json:"processingStatus,omitempty" bson:"processingStatus,omitempty"` // "processing"/ "done"
+}
+
+func (acps *AttackChainScanStatus) GetCustomerGUID() string {
+	return acps.CustomerGUID
+}
+
+func (acfs *AttackChainFirstSeen) GetCustomerGUID() string {
+	return acfs.CustomerGUID
+}
