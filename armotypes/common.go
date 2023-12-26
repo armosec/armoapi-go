e6f3bb09785afa933f4e43632398e7c588310e1e
rinao12 <121787861+rinao12@users.noreply.github.com>
2023-12-26 15:19:32 +0200

    SUB-3621 - CR fixes

--- armotypes/common.go
+++ armotypes/common.go
@@ -13,33 +13,3 @@ const (
 	RiskFactorDataAccess     RiskFactor = "Data access"
 	RiskFactorHostAccess     RiskFactor = "Host access"
 )
-
-var RiskFactorMapping = map[string]RiskFactor{
-	"C-0256": RiskFactorInternetFacing,
-	"C-0046": RiskFactorPrivileged,
-	"C-0057": RiskFactorPrivileged,
-	"C-0255": RiskFactorSecretAccess,
-	"C-0257": RiskFactorDataAccess,
-	"C-0038": RiskFactorHostAccess,
-	"C-0041": RiskFactorHostAccess,
-	"C-0044": RiskFactorHostAccess,
-	"C-0048": RiskFactorHostAccess,
-}
-
-// todo : verify where to add it
-
-// GetRiskFactors returns a list of unique risk factors for given control IDs.
-func GetRiskFactors(controlIDs []string) []RiskFactor {
-	riskFactorSet := make(map[RiskFactor]bool)
-	for _, id := range controlIDs {
-		if riskFactor, exists := RiskFactorMapping[id]; exists {
-			riskFactorSet[riskFactor] = true
-		}
-	}
-
-	var riskFactors []RiskFactor
-	for riskFactor := range riskFactorSet {
-		riskFactors = append(riskFactors, riskFactor)
-	}
-	return riskFactors
-}
