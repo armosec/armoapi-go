757c589ae2a81a1cb18302f5afa1de09916c6e3a
RinaO1234 <45063691+RinaO1234@users.noreply.github.com>
2023-12-27 12:37:28 +0200

    Sub 3621 vulnerabilities + workloads structs +  enrichments  (#190)
    * SUB-3621 - vuln structs
    
    * SUB-3621 - vuln structs
    
    * SUB-3621 - vuln structs
    
    * SUB-3621 - risk factors
    
    * SUB-3621 - workloads details
    
    * SUB-3621 - Added tests
    
    * SUB-3621 - Added tests
    
    * SUB-3621 - Added tests
    
    * SUB-3621 - CR fixes
    
    * SUB-3621 - CR fixes
    
    * SUB-3621 - CR fixes
    
    * SUB-3621 - CR fixes
    
    * SUB-3621 - CR fixes
    
    * SUB-3621 - CR fixes
    
    * SUB-3621 - CR fixes
    
    * SUB-3621 - CR fixes
    
    * SUB-3621 - CR fixes
    
    ---------
    
    Co-authored-by: rinao12 <121787861+rinao12@users.noreply.github.com>

--- armotypes/common.go
+++ armotypes/common.go
@@ -3,3 +3,13 @@ package armotypes
 // swagger:strfmt uuid4
 // Example: 0f42fbe3-d81e-444d-8cc7-bc892c7623e9
 type GUID string
+
+type RiskFactor string
+
+const (
+	RiskFactorInternetFacing RiskFactor = "Internet facing"
+	RiskFactorPrivileged     RiskFactor = "Privileged"
+	RiskFactorSecretAccess   RiskFactor = "Secret access"
+	RiskFactorDataAccess     RiskFactor = "Data access"
+	RiskFactorHostAccess     RiskFactor = "Host access"
+)
