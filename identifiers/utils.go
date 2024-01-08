df1c9685ae75bf8c0b9538e5ffed688237521377
kooomix <eranm@armosec.io>
2024-01-08 13:59:05 +0200

    cakc resource hash

--- identifiers/utils.go
+++ identifiers/utils.go
@@ -3,6 +3,7 @@ package identifiers
 import (
 	"fmt"
 	"hash/fnv"
+	"strings"
 )
 
 // CalcHashFNV calculates the hash (FNV) of the string
@@ -10,4 +11,10 @@ func CalcHashFNV(id string) string {
 	hasher := fnv.New64a()
 	hasher.Write([]byte(id))
 	return fmt.Sprintf("%v", hasher.Sum64())
+}
+
+func CalcResourceHashFNV(customerGUID, cluster, kind, name, namespace, apiVersion string) string {
+	strLower := strings.ToLower(fmt.Sprintf("%s/%s/%s/%s/%s/%s", customerGUID, cluster, kind, name, namespace, apiVersion))
+	return CalcHashFNV(strLower)
+
 }
