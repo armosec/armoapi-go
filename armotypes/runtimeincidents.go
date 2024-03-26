356340e94e88eaac2e65c7a77897ad1eded9c0b3
Bezalel Brandwine <bez@armosec.io>
2024-03-26 17:02:28 +0200

    Update struct field tags in runtimeincidents.go
    Signed-off-by: Bezalel Brandwine <bez@armosec.io>

--- armotypes/runtimeincidents.go
+++ armotypes/runtimeincidents.go
@@ -43,43 +43,43 @@ type RuntimeIncidentResource struct {
 }
 
 type RuleAlert struct {
-	Severity       int    `json:"severity,omitempty"`    // PriorityToStatus(failedRule.Priority()),
-	ProcessName    string `json:"processName,omitempty"` // failedRule.Event().Comm,
-	FixSuggestions string `json:"fixSuggestions,omitempty"`
-	PID            uint32 `json:"pid,omitempty"`
-	PPID           uint32 `json:"ppid,omitempty"` //  Parent Process ID
-	UID            uint32 `json:"uid,omitempty"`  // User ID of the process
-	GID            uint32 `json:"gid,omitempty"`  // Group ID of the process
+	Severity       int    `json:"severity,omitempty" bson:"severity,omitempty"`       // PriorityToStatus(failedRule.Priority()),
+	ProcessName    string `json:"processName,omitempty" bson:"processName,omitempty"` // failedRule.Event().Comm,
+	FixSuggestions string `json:"fixSuggestions,omitempty" bson:"fixSuggestions,omitempty"`
+	PID            uint32 `json:"pid,omitempty" bson:"pid,omitempty"`   // Process ID
+	PPID           uint32 `json:"ppid,omitempty" bson:"ppid,omitempty"` //  Parent Process ID
+	UID            uint32 `json:"uid,omitempty" bson:"uid,omitempty"`   // User ID of the process
+	GID            uint32 `json:"gid,omitempty" bson:"gid,omitempty"`   // Group ID of the process
 }
 
 type MalwareAlert struct {
-	MalwareName        string `json:"malwareName,omitempty"`
-	MalwareDescription string `json:"malwareDescription,omitempty"`
+	MalwareName        string `json:"malwareName,omitempty" bson:"malwareName,omitempty"`
+	MalwareDescription string `json:"malwareDescription,omitempty" bson:"malwareDescription,omitempty"`
 	// Path to the file that was infected
-	Path string `json:"path,omitempty"`
+	Path string `json:"path,omitempty" bson:"path,omitempty"`
 	// Hash of the file that was infected
-	Hash string `json:"hash,omitempty"`
+	Hash string `json:"hash,omitempty" bson:"hash,omitempty"`
 	// Size of the file that was infected
-	Size string `json:"size,omitempty"`
+	Size string `json:"size,omitempty" bson:"size,omitempty"`
 	// Is part of the image
-	IsPartOfImage bool `json:"isPartOfImage,omitempty"`
+	IsPartOfImage bool `json:"isPartOfImage,omitempty" bson:"isPartOfImage,omitempty"`
 	// K8s resource that was infected
-	Resource schema.GroupVersionResource `json:"resource,omitempty"`
+	Resource schema.GroupVersionResource `json:"resource,omitempty" bson:"resource,omitempty"`
 	// K8s container image that was infected
-	ContainerImage string `json:"containerImage,omitempty"`
+	ContainerImage string `json:"containerImage,omitempty" bson:"containerImage,omitempty"`
 }
 
 type RuntimeAlert struct {
 	RuleAlert     `json:",inline"`
 	MalwareAlert  `json:",inline"`
-	RuleName      string    `json:"ruleName"`
-	RuleID        string    `json:"ruleID"`
-	Message       string    `json:"message"`
-	ContainerID   string    `json:"containerID,omitempty"`
-	ContainerName string    `json:"containerName,omitempty"`
-	PodNamespace  string    `json:"podNamespace,omitempty"`
-	PodName       string    `json:"podName,omitempty"`
-	HostName      string    `json:"hostName"`
-	NodeName      string    `json:"nodeName"`
-	Timestamp     time.Time `json:"timestamp"`
+	RuleName      string    `json:"ruleName" bson:"ruleName"`
+	RuleID        string    `json:"ruleID" bson:"ruleID"`
+	Message       string    `json:"message" bson:"message"`
+	ContainerID   string    `json:"containerID,omitempty" bson:"containerID,omitempty"`
+	ContainerName string    `json:"containerName,omitempty" bson:"containerName,omitempty"`
+	PodNamespace  string    `json:"podNamespace,omitempty" bson:"podNamespace,omitempty"`
+	PodName       string    `json:"podName,omitempty" bson:"podName,omitempty"`
+	HostName      string    `json:"hostName" bson:"hostName"`
+	NodeName      string    `json:"nodeName" bson:"nodeName"`
+	Timestamp     time.Time `json:"timestamp" bson:"timestamp"`
 }
