2da1c02fe06ef9b1b707daecc041ed25ba63f248
Amit Schendel <amitschendel@gmail.com>
2024-04-02 19:01:33 +0300

    Adding correct full types for KDR
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- armotypes/runtimeincidents.go
+++ armotypes/runtimeincidents.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/armosec/armoapi-go/identifiers"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 type IncidentCategory string
@@ -12,6 +11,13 @@ type IncidentCategory string
 const (
 	RuntimeIncidentCategoryMalware IncidentCategory = "Malware"
 	RuntimeIncidentCategoryAnomaly IncidentCategory = "Anomaly"
+)
+
+type AlertType int
+
+const (
+	AlertTypeRule AlertType = iota
+	AlertTypeMalware
 )
 
 type RuntimeIncident struct {
@@ -43,43 +49,76 @@ type RuntimeIncidentResource struct {
 }
 
 type RuleAlert struct {
-	Severity       int    `json:"severity,omitempty" bson:"severity,omitempty"`       // PriorityToStatus(failedRule.Priority()),
-	ProcessName    string `json:"processName,omitempty" bson:"processName,omitempty"` // failedRule.Event().Comm,
+	// Rule Name
+	RuleName string `json:"ruleName,omitempty" bson:"ruleName,omitempty"`
+	// Rule ID
+	RuleID string `json:"ruleID,omitempty" bson:"ruleID,omitempty"`
+	// Severity of the alert
+	Severity int `json:"severity,omitempty" bson:"severity,omitempty"`
+	// Process name
+	ProcessName string `json:"processName,omitempty" bson:"processName,omitempty"`
+	// Command line
+	CommandLine string `json:"commandLine,omitempty" bson:"commandLine,omitempty"`
+	// Fix suggestions
 	FixSuggestions string `json:"fixSuggestions,omitempty" bson:"fixSuggestions,omitempty"`
-	PID            uint32 `json:"pid,omitempty" bson:"pid,omitempty"`   // Process ID
-	PPID           uint32 `json:"ppid,omitempty" bson:"ppid,omitempty"` //  Parent Process ID
-	UID            uint32 `json:"uid,omitempty" bson:"uid,omitempty"`   // User ID of the process
-	GID            uint32 `json:"gid,omitempty" bson:"gid,omitempty"`   // Group ID of the process
+	// Parent Process ID
+	PPID uint32 `json:"ppid,omitempty" bson:"ppid,omitempty"`
+	// PPIDComm - Parent Process Name
+	PPIDComm string `json:"ppidComm,omitempty" bson:"ppidComm,omitempty"`
+	// Is part of the image
+	IsPartOfImage bool `json:"isPartOfImage,omitempty" bson:"isPartOfImage,omitempty"`
+	// MD5 hash of the file that was infected
+	MD5Hash string `json:"md5Hash,omitempty" bson:"md5Hash,omitempty"`
+	// SHA1 hash of the file that was infected
+	SHA1Hash string `json:"sha1Hash,omitempty" bson:"sha1Hash,omitempty"`
+	// SHA256 hash of the file that was infected
+	SHA256Hash string `json:"sha256Hash,omitempty" bson:"sha256Hash,omitempty"`
 }
 
 type MalwareAlert struct {
-	MalwareName        string `json:"malwareName,omitempty" bson:"malwareName,omitempty"`
+	// Name of the malware
+	MalwareName string `json:"malwareName,omitempty" bson:"malwareName,omitempty"`
+	// Description of the malware
 	MalwareDescription string `json:"malwareDescription,omitempty" bson:"malwareDescription,omitempty"`
 	// Path to the file that was infected
 	Path string `json:"path,omitempty" bson:"path,omitempty"`
-	// Hash of the file that was infected
-	Hash string `json:"hash,omitempty" bson:"hash,omitempty"`
+	// MD5 hash of the file that was infected
+	MD5Hash string `json:"md5Hash,omitempty" bson:"md5Hash,omitempty"`
+	// SHA1 hash of the file that was infected
+	SHA1Hash string `json:"sha1Hash,omitempty" bson:"sha1Hash,omitempty"`
+	// SHA256 hash of the file that was infected
+	SHA256Hash string `json:"sha256Hash,omitempty" bson:"sha256Hash,omitempty"`
 	// Size of the file that was infected
 	Size string `json:"size,omitempty" bson:"size,omitempty"`
 	// Is part of the image
 	IsPartOfImage bool `json:"isPartOfImage,omitempty" bson:"isPartOfImage,omitempty"`
-	// K8s resource that was infected
-	Resource schema.GroupVersionResource `json:"resource,omitempty" bson:"resource,omitempty"`
-	// K8s container image that was infected
-	ContainerImage string `json:"containerImage,omitempty" bson:"containerImage,omitempty"`
+	// Severity of the malware
+	Severity int `json:"severity,omitempty" bson:"severity,omitempty"`
+	// Parent Process ID of the process that was infected
+	PPID uint32 `json:"ppid,omitempty" bson:"ppid,omitempty"`
+	// Parent Process Name of the process that was infected
+	PPIDComm string `json:"ppidComm,omitempty" bson:"ppidComm,omitempty"`
+	// Command Line of the process that was infected
+	CommandLine string `json:"commandLine,omitempty" bson:"commandLine,omitempty"`
+	// Fix suggestions
+	FixSuggestions string `json:"fixSuggestions,omitempty" bson:"fixSuggestions,omitempty"`
 }
 
 type RuntimeAlert struct {
 	RuleAlert     `json:",inline"`
 	MalwareAlert  `json:",inline"`
-	RuleName      string    `json:"ruleName" bson:"ruleName"`
-	RuleID        string    `json:"ruleID" bson:"ruleID"`
+	AlertType     AlertType `json:"alertType" bson:"alertType"`
 	Message       string    `json:"message" bson:"message"`
 	ContainerID   string    `json:"containerID,omitempty" bson:"containerID,omitempty"`
 	ContainerName string    `json:"containerName,omitempty" bson:"containerName,omitempty"`
-	PodNamespace  string    `json:"podNamespace,omitempty" bson:"podNamespace,omitempty"`
+	Namespace     string    `json:"namespace,omitempty" bson:"namespace,omitempty"`
 	PodName       string    `json:"podName,omitempty" bson:"podName,omitempty"`
+	HostNetwork   bool      `json:"hostNetwork,omitempty" bson:"hostNetwork,omitempty"`
+	Image         string    `json:"image,omitempty" bson:"image,omitempty"`
+	ImageDigest   string    `json:"imageDigest,omitempty" bson:"imageDigest,omitempty"`
 	HostName      string    `json:"hostName" bson:"hostName"`
 	NodeName      string    `json:"nodeName" bson:"nodeName"`
+	WorkloadName  string    `json:"workloadName" bson:"workloadName"`
+	WorkloadKind  string    `json:"workloadKind" bson:"workloadKind"`
 	Timestamp     time.Time `json:"timestamp" bson:"timestamp"`
 }
