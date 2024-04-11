652d58e28df9600189928835cd05cd5ccbd88247
Avraham Shalev <8184528+avrahams@users.noreply.github.com>
2024-04-11 16:03:31 +0300

    add type to entity map
    Signed-off-by: Avraham Shalev <8184528+avrahams@users.noreply.github.com>

--- notifications/integrationref.go
+++ notifications/integrationref.go
@@ -167,8 +167,8 @@ func (e *EntityIdentifiers) Validate() error {
 }
 
 func (e *EntityIdentifiers) ToMap() map[string]string {
-	//avoid empty values
 	entityMap := make(map[string]string)
+	entityMap[identifiers.AttributeType] = string(e.Type)
 	if e.Cluster != "" {
 		entityMap[identifiers.AttributeCluster] = e.Cluster
 	}
