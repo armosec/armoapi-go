eaeed28198dff729a0b120896140cfb2f5f1f773
Avraham Shalev <8184528+avrahams@users.noreply.github.com>
2024-05-01 13:29:44 +0300

    ne seperator
    Signed-off-by: Avraham Shalev <8184528+avrahams@users.noreply.github.com>

--- armotypes/pagination_structs.go
+++ armotypes/pagination_structs.go
@@ -35,6 +35,7 @@ const (
 	V2ListSubQuerySeparator = "&"
 	V2ListSortTypeSeparator = ":"
 	V2ListEscapeChar        = "\\"
+	V2ListAndValueSeparator = ";" // for not equal operator ("field": "1;2|notequal" -> field != 1 AND field != 2)
 )
 
 type QueryScopeParams struct {
