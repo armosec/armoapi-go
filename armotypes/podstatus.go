c031db7a111384e7b3e3825a66345037d6ad0b07
Matthias Bertschy <matthias.bertschy@gmail.com>
2024-05-22 15:25:44 +0200

    move some fields from PodStatus to PodContainer
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- armotypes/podstatus.go
+++ armotypes/podstatus.go
@@ -6,41 +6,38 @@ import (
 )
 
 type PodStatus struct {
-	CustomerGUID        string         `json:"customerGUID"`
-	Cluster             string         `json:"cluster"`
-	ResourceHash        string         `json:"resourceHash"`
-	ResourceVersion     string         `json:"resourceVersion"`
-	Name                string         `json:"name"`
-	Namespace           string         `json:"namespace"`
-	NodeName            string         `json:"nodeName"`
-	App                 string         `json:"app"`
-	Phase               string         `json:"phase"`
-	CurrentState        string         `json:"currentState"`
-	LastStateExitCode   int            `json:"lastStateExitCode"`
-	LastStateFinishedAt time.Time      `json:"lastStateFinishedAt"`
-	LastStateStartedAt  time.Time      `json:"lastStateStartedAt"`
-	LastStateReason     string         `json:"lastStateReason"`
-	LastStateMessage    string         `json:"lastStateMessage"`
-	RestartCount        int            `json:"restartCount"`
-	CreationTimestamp   time.Time      `json:"creationTimestamp"`
-	Containers          []PodContainer `json:"containers,omitempty"`
-	InitContainers      []PodContainer `json:"initContainers,omitempty"`
-	EphemeralContainers []PodContainer `json:"ephemeralContainers,omitempty"`
-
-	HasFinalApplicationProfile bool `json:"hasFinalApplicationProfile"`
-
-	HasApplicableRuleBindings bool `json:"hasApplicableRuleBindings"`
-
-	HasRelevancyCalculating bool `json:"hasRelevancyCalculating"`
-
-	IsKDRMonitored bool `json:"isKDRMonitored"`
+	CustomerGUID               string         `json:"customerGUID"`
+	Cluster                    string         `json:"cluster"`
+	ResourceHash               string         `json:"resourceHash"`
+	ResourceVersion            string         `json:"resourceVersion"`
+	Name                       string         `json:"name"`
+	Namespace                  string         `json:"namespace"`
+	NodeName                   string         `json:"nodeName"`
+	App                        string         `json:"app"`
+	Phase                      string         `json:"phase"`
+	CurrentState               string         `json:"currentState"`
+	LastStateReason            string         `json:"lastStateReason"`
+	LastStateMessage           string         `json:"lastStateMessage"`
+	LastStateTransitionTime    time.Time      `json:"lastStateTransitionTime"`
+	CreationTimestamp          time.Time      `json:"creationTimestamp"`
+	Containers                 []PodContainer `json:"containers,omitempty"`
+	InitContainers             []PodContainer `json:"initContainers,omitempty"`
+	EphemeralContainers        []PodContainer `json:"ephemeralContainers,omitempty"`
+	HasFinalApplicationProfile bool           `json:"hasFinalApplicationProfile"`
+	HasApplicableRuleBindings  bool           `json:"hasApplicableRuleBindings"`
+	HasRelevancyCalculating    bool           `json:"hasRelevancyCalculating"`
+	IsKDRMonitored             bool           `json:"isKDRMonitored"`
 }
 
 type PodContainer struct {
-	Name           string `json:"name"`
-	Image          string `json:"image"`
-	IsKDRMonitored bool   `json:"isKDRMonitored"`
-	CurrentState   string `json:"currentState"`
+	Name                string    `json:"name"`
+	Image               string    `json:"image"`
+	IsKDRMonitored      bool      `json:"isKDRMonitored"`
+	CurrentState        string    `json:"currentState"`
+	LastStateExitCode   int       `json:"lastStateExitCode"`
+	LastStateFinishedAt time.Time `json:"lastStateFinishedAt"`
+	LastStateStartedAt  time.Time `json:"lastStateStartedAt"`
+	RestartCount        int       `json:"restartCount"`
 }
 
 func (ps *PodStatus) GetMonitoredContainers() []PodContainer {
