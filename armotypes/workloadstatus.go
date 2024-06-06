9f1556010a38f21df6c916e256cd745115ce6ac8
YiscahLevySilas1 <yiscahls@armosec.io>
2024-06-06 09:26:30 +0300

    add clustername
    Signed-off-by: YiscahLevySilas1 <yiscahls@armosec.io>

--- armotypes/workloadstatus.go
+++ armotypes/workloadstatus.go
@@ -3,5 +3,6 @@ package armotypes
 type WorkloadStatus struct {
 	ResourceHash     string `json:"resourceHash"`
 	CustomerGUID     string `json:"customerGUID"`
+	ClusterName      string `json:"clusterName"`
 	IsInternetFacing *bool  `json:"isInternetFacing"`
 }
