7f4394eb82f603fe57171d3d966bf2301cf5affc
YiscahLevySilas1 <yiscahls@armosec.io>
2024-06-10 13:15:00 +0300

    Revert "add fields to WorkloadStatus"
    This reverts commit 92fd0e788cfedf9ed48f4231fe7663ef1a5a454e.
    
    Signed-off-by: YiscahLevySilas1 <yiscahls@armosec.io>

--- armotypes/workloadstatus.go
+++ armotypes/workloadstatus.go
@@ -1,16 +1,8 @@
 package armotypes
 
 type WorkloadStatus struct {
-	WorkloadName        string   `json:"workloadName"`
-	Namespace           string   `json:"namespace"`
-	Kind                string   `json:"kind"`
-	ClusterName         string   `json:"clusterName"`
-	CustomerGUID        string   `json:"customerGUID"`
-	ResourceHash        string   `json:"resourceHash"`
-	ResourceHashNumeric float64  `json:"resourceHashNumeric"`
-	workloadLabelsCount int      `json:"workloadLabelsCount"`
-	riskFactorsCount    int      `json:"riskFactorsCount"`
-	WorkloadLabels      []string `json:"workloadLabels"`
-	RiskFactors         []string `json:"riskFactors"`
-	IsInternetFacing    *bool    `json:"isInternetFacing"`
+	ResourceHash     string `json:"resourceHash"`
+	CustomerGUID     string `json:"customerGUID"`
+	ClusterName      string `json:"clusterName"`
+	IsInternetFacing *bool  `json:"isInternetFacing"`
 }
