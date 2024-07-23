7d70158e9fc30da3d3359efe32b7c5673c2e3a00
Amir Malka <amirm@armosec.io>
2024-07-23 16:25:39 +0300

    added workload statuses (#349)
    Signed-off-by: Amir Malka <amirm@armosec.io>

--- armotypes/workloadstatus.go
+++ armotypes/workloadstatus.go
@@ -1,8 +1,9 @@
 package armotypes
 
 type WorkloadStatus struct {
-	ResourceHash     string `json:"resourceHash"`
-	CustomerGUID     string `json:"customerGUID"`
-	ClusterName      string `json:"clusterName"`
-	IsInternetFacing *bool  `json:"isInternetFacing"`
+	ResourceHash     string   `json:"resourceHash"`
+	CustomerGUID     string   `json:"customerGUID"`
+	ClusterName      string   `json:"clusterName"`
+	IsInternetFacing *bool    `json:"isInternetFacing"`
+	RiskFactors      []string `json:"riskFactors"`
 }
