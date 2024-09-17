cdd2b7eb3aae1c70ac5435ddc8d25160a0913ed3
idohu <idoh@armosec.io>
2024-09-17 14:43:18 +0300

    add CSPM shared consts
    Signed-off-by: idohu <idoh@armosec.io>

---
+++ armotypes/cloudposturetypes.go
@@ -0,0 +1,23 @@
+package armotypes
+
+var SeverityToInt = map[string]int{
+	"critical":   500,
+	"high":       400,
+	"medium":     300,
+	"low":        200,
+	"Negligible": 100,
+}
+
+var CheckStatusToInt = map[string]int{
+	"EMPTY":   -1,
+	"MANUAL":  0,
+	"FAIL":    1,
+	"PASS":    2,
+	"SKIPPED": 3,
+}
+
+var ScanStatusToInt = map[string]int{
+	"FAILED":     1,
+	"INPROGRESS": 2,
+	"SUCCESS":    3,
+}
