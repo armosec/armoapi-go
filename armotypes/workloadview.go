7fdc6fa8c95b61af8f378fd2592db97c42b10f97
jnathangreeg <jonathang@armosec.io>
2025-05-20 14:59:22 +0300

    workload views
    Signed-off-by: jnathangreeg <jonathang@armosec.io>

---
+++ armotypes/workloadview.go
@@ -0,0 +1,18 @@
+package armotypes
+
+import "time"
+
+type WorkloadViews struct {
+	WorkloadName      string     `json:"workloadName"`
+	Kind              string     `json:"kind"`
+	Cluster           string     `json:"cluster"`
+	AccountID         string     `json:"accountId"`
+	Region            string     `json:"region"`
+	Provider          string     `json:"provider"`
+	Namespace         string     `json:"namespace"`
+	CreationTimestamp *time.Time `json:"CreationTimestamp,omitempty"`
+	CompletionStatus  string     `json:"CompletionStatus,omitempty"`
+	Status            string     `json:"Status,omitempty"`
+	LearningPeriod    string     `json:"learningPeriod,omitempty"`
+	RiskFactors       []string   `json:"riskFactors,omitempty"`
+}
