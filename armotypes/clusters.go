38f59f511daff87ec59f5e81864766aaff6ff29a
kooomix <eranm@armosec.io>
2025-05-21 09:12:20 +0300

    Enhance ClusterInfo struct by adding ConnectionTime and StatusChangeTime fields

--- armotypes/clusters.go
+++ armotypes/clusters.go
@@ -3,19 +3,21 @@ package armotypes
 import "time"
 
 type ClusterInfo struct {
-	Cluster        string       `json:"cluster"`
-	NodeCount      int          `json:"nodeCount"`
-	CPUSum         int          `json:"cpuSum"`
-	CloudProvider  string       `json:"cloudProvider"`
-	HelmVersion    string       `json:"helmVersion"`
-	ClusterVersion string       `json:"clusterVersion"`
-	LastReportTime *time.Time   `json:"lastReportTime,omitempty"`
-	LastKeepAlive  *time.Time   `json:"lastKeepAlive,omitempty"`
-	CreatedAt      *time.Time   `json:"createdAt,omitempty"`
-	IsConnected    bool         `json:"isConnected"`
-	Capabilities   []Capability `json:"capabilities,omitempty"`
-	Status         string       `json:"status,omitempty"`
-	FailedFeatures []string     `json:"failedFeatures,omitempty"`
+	Cluster          string       `json:"cluster"`
+	NodeCount        int          `json:"nodeCount"`
+	CPUSum           int          `json:"cpuSum"`
+	CloudProvider    string       `json:"cloudProvider"`
+	HelmVersion      string       `json:"helmVersion"`
+	ClusterVersion   string       `json:"clusterVersion"`
+	LastReportTime   *time.Time   `json:"lastReportTime,omitempty"`
+	LastKeepAlive    *time.Time   `json:"lastKeepAlive,omitempty"`
+	CreatedAt        *time.Time   `json:"createdAt,omitempty"`
+	IsConnected      bool         `json:"isConnected"`
+	Capabilities     []Capability `json:"capabilities,omitempty"`
+	Status           string       `json:"status,omitempty"`
+	FailedFeatures   []string     `json:"failedFeatures,omitempty"`
+	ConnectionTime   *time.Time   `json:"connectionTime,omitempty"`
+	StatusChangeTime *time.Time   `json:"statusChangeTime,omitempty"`
 }
 
 type Capability struct {
