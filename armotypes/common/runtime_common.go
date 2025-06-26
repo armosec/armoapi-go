6e875b2a08db3003f61c8c14629916946e63acfa
shanyl9 <shanyl@armosec.io>
2025-06-26 18:12:50 +0300

    add flatten method

--- armotypes/common/runtime_common.go
+++ armotypes/common/runtime_common.go
@@ -18,12 +18,11 @@ type NetworkEntity struct {
 }
 
 type HttpEntity struct {
-	Method   string            `json:"method,omitempty" bson:"method,omitempty"`
-	Domain   string            `json:"domain,omitempty" bson:"domain,omitempty"`
-	Endpoint string            `json:"endpoint,omitempty" bson:"endpoint,omitempty"`
-	Header   map[string]string `json:"header,omitempty" bson:"header,omitempty"`
-	Proto    string            `json:"proto,omitempty" bson:"proto,omitempty"`
-	Payload  string            `json:"payload,omitempty" bson:"payload,omitempty"`
+	Method   string `json:"method,omitempty" bson:"method,omitempty"`
+	Domain   string `json:"domain,omitempty" bson:"domain,omitempty"`
+	Endpoint string `json:"endpoint,omitempty" bson:"endpoint,omitempty"`
+	Proto    string `json:"proto,omitempty" bson:"proto,omitempty"`
+	Payload  string `json:"payload,omitempty" bson:"payload,omitempty"`
 }
 
 type CloudAPIEntity struct {
@@ -39,4 +38,35 @@ type Identifiers struct {
 	Network  *NetworkEntity  `json:"network,omitempty" bson:"network,omitempty"`
 	Http     *HttpEntity     `json:"http,omitempty" bson:"http,omitempty"`
 	CloudAPI *CloudAPIEntity `json:"cloudAPI,omitempty" bson:"cloudAPI,omitempty"`
+}
+
+func (identifiers *Identifiers) Flatten() map[string]string {
+	identifiers_map := make(map[string]string)
+
+	if identifiers.Process != nil {
+		identifiers_map["process.name"] = identifiers.Process.Name
+		identifiers_map["process.commandLine"] = identifiers.Process.CommandLine
+	}
+	if identifiers.File != nil {
+		identifiers_map["file.path"] = identifiers.File.Path
+		identifiers_map["file.directory"] = identifiers.File.Directory
+	}
+	if identifiers.Network != nil {
+		identifiers_map["network.domain"] = identifiers.Network.Domain
+		identifiers_map["network.address"] = identifiers.Network.Address
+		identifiers_map["network.dstPort"] = identifiers.Network.DstPort
+		identifiers_map["network.protocol"] = identifiers.Network.Protocol
+	}
+	if identifiers.Http != nil {
+		identifiers_map["http.method"] = identifiers.Http.Method
+		identifiers_map["http.domain"] = identifiers.Http.Domain
+		identifiers_map["http.endpoint"] = identifiers.Http.Endpoint
+	}
+	if identifiers.CloudAPI != nil {
+		identifiers_map["cloudAPI.service"] = identifiers.CloudAPI.Service
+		identifiers_map["cloudAPI.action"] = identifiers.CloudAPI.Action
+		identifiers_map["cloudAPI.resource"] = identifiers.CloudAPI.Resource
+		identifiers_map["cloudAPI.user"] = identifiers.CloudAPI.User
+	}
+	return identifiers_map
 }
