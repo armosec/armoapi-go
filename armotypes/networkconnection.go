c8dbcb20651468c2bfecf69bdf68893105590a5b
jnathangreeg <jonathang@armosec.io>
2025-07-03 17:47:01 +0300

    Add AggregatedNetworkConnection struct to networkconnection.go for enhanced network data representation

--- armotypes/networkconnection.go
+++ armotypes/networkconnection.go
@@ -11,3 +11,11 @@ type NetworkConnection struct {
 	EndpointWorkloadNamespace *string `json:"endpointWorkloadNamespace,omitempty"`
 	EndpointWorkloadKind      *string `json:"endpointWorkloadKind,omitempty"`
 }
+
+type AggregatedNetworkConnection struct {
+	DNSName     string   `json:"dnsName"`
+	Direction   string   `json:"direction"`
+	Port        int32    `json:"port"`
+	Protocol    string   `json:"protocol"`
+	IPAddresses []string `json:"ipAddresses"`
+}
