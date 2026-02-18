764159c9b0407c3ecbbdafda515920bc540e3332
itsmeital <meitalr@armosec.io>
2026-02-18 15:43:45 +0200

    add inventory type
    Signed-off-by: itsmeital <meitalr@armosec.io>

---
+++ armotypes/inventory.go
@@ -0,0 +1,21 @@
+package armotypes
+
+import "time"
+
+type Inventory struct {
+	WorkloadName       string     `json:"workloadName"`
+	Kind               string     `json:"kind"` // will be deprecated in the future after type is introduced
+	Type               string     `json:"type"`
+	Cluster            string     `json:"cluster"`
+	AccountID          string     `json:"accountId"`
+	Region             string     `json:"region"`
+	Provider           string     `json:"provider"`
+	Namespace          string     `json:"namespace"`
+	CreationTimestamp  *time.Time `json:"creationTimestamp,omitempty"`
+	CompletionStatus   string     `json:"completionStatus,omitempty"`
+	Status             string     `json:"status,omitempty"`
+	LearningPeriod     string     `json:"learningPeriod,omitempty"`
+	RiskFactors        []string   `json:"riskFactors,omitempty"`
+	LearningPercentage *int       `json:"learningPercentage,omitempty"`
+	HostName           string     `json:"hostName,omitempty"`
+}
